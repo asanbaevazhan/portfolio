0d227211fbeb4688fefa1e764857f0decabd1970
Aizhan <asanbaevazhan@gmail.com>
2023-01-26 11:09:16 +0600

    init

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+import Keyboard from '../assets/projects/1.png'
+import Todo from '../assets/projects/4.png'
+import WeatherApp from '../assets/projects/5.png'
+import HooBank from '../assets/projects/3.png'
+import Constr from '../assets/projects/2.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Landing Page",
+        image:HooBank,
+        github:"https://github.com/asanbaevazhan/bank-modern-app",
+        live:"https://asanbaevazhan.github.io/bank-modern-app/",
+    },
+    {
+        id:2,
+        name:"Weather App",
+        image:WeatherApp,
+        github:"https://github.com/asanbaevazhan/react-api-app",
+        live:"https://asanbaevazhan.github.io/react-api-app/",
+    },
+    {
+        id:3,
+        name:"TODO List",
+        image:Todo,
+        github:"https://github.com/asanbaevazhan/react-todo-crud",
+        live:"https://asanbaevazhan.github.io/react-todo-crud/",
+    },
+    {
+        id:4,
+        name:"Virtual Keyboard",
+        image:Keyboard,
+        github:"https://github.com/asanbaevazhan/virtual-keyboard",
+        live:"https://asanbaevazhan.github.io/virtual-keyboard/",
+    },
+    {
+        id:5,
+        name:"Construction Project",
+        image:Constr,
+        github:"https://github.com/asanbaevazhan/construction-project",
+        live:"https://asanbaevazhan.github.io/construction-project/",
+    },
+
+
+]
